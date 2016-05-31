98f3087290d2a14c339ff23ffe1ff8359e9e984b
acioni <andreacioni@AndreaMacBookAir.local>
2016-05-31 09:02:30 +0200

    started integration of kp2a fix keelink php update user query

---
+++ android/KeelinkApp/src/main/java/it/andreacioni/kp2a/plugin/keelink/Kp2aAccessReceiver.java
@@ -0,0 +1,10 @@
+package it.andreacioni.kp2a.plugin.keelink;
+
+import keepass2android.pluginsdk.PluginAccessBroadcastReceiver;
+
+/**
+ * Created by andreacioni on 30/05/16.
+ */
+
+public class Kp2aAccessReceiver extends PluginAccessBroadcastReceiver {
+}
---
+++ android/KeelinkApp/src/main/java/it/andreacioni/kp2a/plugin/keelink/Kp2aActionReceiver.java
@@ -0,0 +1,10 @@
+package it.andreacioni.kp2a.plugin.keelink;
+
+import keepass2android.pluginsdk.PluginActionBroadcastReceiver;
+
+/**
+ * Created by andreacioni on 30/05/16.
+ */
+
+public class Kp2aActionReceiver extends PluginActionBroadcastReceiver {
+}
